4f454866a16bb643d4d4959a3a997c5e1df43773
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2023-12-27 23:36:46 +0200

    created course class

--- file.py
+++
@@ -1,4 +0,0 @@
-def write_code():
-  print('Writter Function')
-if __name__ == '__main__':
-  write_code()
---
+++ main.py
@@ -0,0 +1,5 @@
+from Course import Course
+
+if __name__ == '__main__':
+  c1 = Course(3, ['PHP', 'Python', 'Node.Js'])
+  print(c1.getAllCourseTeachers())
