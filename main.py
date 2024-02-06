62dba5226aafe1f0f2c80a8b55db8f81e0002fb6
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2024-02-06 22:04:02 +0200

    Using Student Entity in main file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from course import Course
+from student import Student
 from teacher import Teacher
 
 if __name__ == '__main__':
   c1 = Course(3, ['PHP', 'Python', 'Node.Js'])
   c2 = Course(2, ['Dart', 'Go'])
   c3 = Course(1, ['React-Native'])
-  t = Teacher(3, [c1, c2, c3])
-  t.getCourses()
+  t1 = Teacher(2, [c1, c2])
+  t2 = Teacher(2, [c2, c3])
+  s = Student(2, [t1, t2])
+  s.getTeachers()
